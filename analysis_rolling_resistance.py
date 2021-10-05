5f62f6c2cd7d1885ed57ead9e5226b7a35d4353a
DbellSchool <54284033+DbellSchool@users.noreply.github.com>
2021-10-05 16:44:47 -0500

    added way to graph

--- analysis_rolling_resistance.py
+++ analysis_rolling_resistance.py
@@ -9,7 +9,44 @@
 # •Do not display anything to the console•The hints for the previous problem apply here as well.
 #
 import numpy
+import matplotlib.pyplot as plt
 
 theda = 0 # angle of incline 
 Crr_array = numpy.linspace(0.01,0.4,25)
+
+w = Crr_array*2
+
+plt.plot(w,Crr_array, '-b')
+
+plt.xlabel("omega [rad/s]")
+plt.ylabel("CRR")
+plt.grid("on")
+#plt.xlim(0,5)
+plt.show()
+
+#############Graphing with SubplotsL
+# import matplotlib.pyplot as plt
+#     print("Starting Task 3")
+#     N = 20
+
+#     x1 = linspace(-5,5,num = N)
+#     y1 = 0.5 *x1**2
+
+#     x2 = linspace(0,100,num = N)
+#     y2 = -x2
+
+#     fig, (ax1, ax2) = plt.subplots(2)
+#     fig.tight_layout(pad=3)
+#     fig.suptitle('Task 3')
+
+#     ax1.plot(x1, y1)
+#     ax1.set_title("Exhibit 1")
+#     ax1.set_xlabel("x")
+#     ax1.set_ylabel("y")
+
+#     ax2.plot(x2, y2)
+#     ax2.set_title("Exhibit 2")
+#     ax2.set_xlabel("Work")
+#     ax2.set_ylabel("Play")
+#     plt.show()
 
