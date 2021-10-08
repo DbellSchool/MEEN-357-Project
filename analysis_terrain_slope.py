e991a269a25a94767f2b1eae7e7d7e71ee37b09a
emmafayee <91642451+emmafayee@users.noreply.github.com>
2021-10-07 20:24:49 -0500

    analysis_terrain_slope

---
+++ analysis_terrain_slope.py
@@ -0,0 +1,35 @@
+import numpy as np
+from math import ceil, log
+from numpy import sign
+import sys
+import matplotlib.pyplot as plt
+from scipy.optimize import root_scalar
+
+
+
+terrain_angle = np.linspace(-10, 35, 25)
+
+Crr = 0.2
+
+L =-100
+R = 100
+
+Ng = get_gear_ratio(rover['wheel_assembly']['speed_reducer'])
+r =rover['wheel_assembly']['wheel']['radius']
+v_max = []
+
+
+for i in terrain_angle:
+    Fwrap = lambda x: F_net( x, [i], rover, planet, [0.2])
+    w = root_scalar(Fwrap,method='bisect', bracket=[L,R])
+    wWheel = w.root/Ng
+    V = wWheel*r
+    v_max.append(V)
+    
+print(v_max)  
+
+plt.plot(v_max,terrain_angle, '-b')
+plt.xlabel("omega [rad/s]")
+plt.ylabel("Terrain Angle [degrees]")
+plt.grid("on")
+plt.show()
